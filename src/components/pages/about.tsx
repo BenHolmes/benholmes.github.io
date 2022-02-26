875ebe16c587e969686a7e2d9654a83661c42457
Benjamin Holmes <ben.holmes1357@gmail.com>
2022-02-26 15:54:25 -0800

    cleaning up app

--- src/components/pages/about.tsx
+++ src/components/pages/about.tsx
@@ -5,20 +5,6 @@ export default function About() {
 
   return (
     <div className="About">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. I love you baby!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <h1>About</h1>
     </div>
   );
