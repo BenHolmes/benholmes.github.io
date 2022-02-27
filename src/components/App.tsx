a0398923360e9b4a3856f3b834b65bd4e96be570
Benjamin Holmes <ben.holmes1357@gmail.com>
2022-02-26 20:49:03 -0800

    Added sass, changed title and icon, updated nav styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from  'react-router-dom';
 import Nav from './navigation/nav';
-import './App.css';
+
 import Home from './pages/home';
 import About from './pages/about';
 import Projects from './pages/projects';
+import './App.module.scss';
 
 export default function App() {
   return (
--- src/components/navigation/nav.tsx
+++ src/components/navigation/nav.tsx
@@ -3,18 +3,19 @@ import { Link } from 'react-router-dom';
 import Home from '../pages/home';
 import Projects from '../pages/projects';
 import About from '../pages/about';
+import ClassNames from './nav.module.scss';
 
 
 export default function Nav() {
 
 
   return (
-    <div className="Nav">
-        <ul>
-            <Link to={'/'}><Home/></Link>
+    <div className={ClassNames.TopBar}>
+        <div className={ClassNames.TopBarLeftHomeButton}><Link to={'/'}><Home/></Link></div>
+        <div className={ClassNames.TopBarRight}>
             <Link to={'/projects'}><Projects/></Link>
             <Link to={'/about'}><About/></Link>
-        </ul>  
+        </div>
     </div>
   );
 }
