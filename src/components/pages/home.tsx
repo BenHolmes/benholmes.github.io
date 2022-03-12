54e4e4568a6e8f3b6529a6b219abdec353709116
Benjamin Holmes <ben.holmes1357@gmail.com>
2022-03-11 23:55:35 -0800

    profile adjustments, basics complete NEEDS IMPROVE

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import profile from '../../assets/images/HolmesProfileSmall.jpeg';
+import styles from './home.module.scss';
 
 export default function Home() {
 
-  const profile = require('../../assets/images/HolmesProfileSmall.jpeg');
+  const profileSm = require('../../assets/images/HolmesProfileSm.jpeg');
+  const profileLg = require('../../assets/images/HolmesProfileLg.jpeg');
 
   return (
-    <div className="Home" style={{display:'flex', flexDirection:'row', overflow:'nowrap'}}>
-      <div style={{display:'flex', padding:'10px'}}>
-        I am Ben Holmes a software developer in the Greater Seattle area specializing in the future resource intensive applications. 
+    <div className={styles.HomeContent}>
+      <div className={styles.TopBarLeft} >
+        <div>
+        I am <text style={{fontWeight: "bold"}}>Ben Holmes</text> a software developer in the Greater Seattle area specializing in the future resource intensive applications. 
         I am currently working at Doosan GridTech as the primary front-end developer of Site Manager 2.0. 
         I am interested in the migration of highly reliable applications movement towards API/Web front-end architecture and how quantum computers could change the future of software development and aid us in our fight against climate change.
+        </div>
       </div>
-      <div style={{display:'flex'}}>
-        <img src={profile} />
+
+      <div className={styles.TopBarRight}>
+
+        <img 
+          //className={styles.detail}
+          src={profileSm} 
+          srcSet={`${profileSm} 400w,
+            ${profileSm} 600w,
+            ${profileLg} 800w,
+            ${profileLg} 1200w`}
+          //sizes ="(min-width:1336px) 75vw, 50vw"
+          alt="Ben Holmes" 
+        />
       </div>
     </div>
   );
