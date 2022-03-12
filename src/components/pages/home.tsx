0c91251ddd37d3676e40193b9dc46bd99f052bb8
Benjamin Holmes <ben.holmes1357@gmail.com>
2022-03-12 11:59:32 -0800

    margin scaling changes

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import profile from '../../assets/images/HolmesProfileSmall.jpeg';
+import { Link } from 'react-router-dom';
+//import profile from '../../assets/images/HolmesProfileSmall.jpeg';
 import styles from './home.module.scss';
 
 export default function Home() {
@@ -10,10 +11,13 @@ export default function Home() {
   return (
     <div className={styles.HomeContent}>
       <div className={styles.TopBarLeft} >
-        <div>
-        I am <text style={{fontWeight: "bold"}}>Ben Holmes</text> a software developer in the Greater Seattle area specializing in the future resource intensive applications. 
-        I am currently working at Doosan GridTech as the primary front-end developer of Site Manager 2.0. 
-        I am interested in the migration of highly reliable applications movement towards API/Web front-end architecture and how quantum computers could change the future of software development and aid us in our fight against climate change.
+        <div className={styles.TopBarLeftContent}>
+          <div>
+          I am <text style={{fontWeight: "bold"}}>Ben Holmes</text> a software developer in the Greater Seattle area specializing in the future resource intensive applications. 
+          I am currently working at Doosan GridTech as the primary front-end developer of Site Manager 2.0. 
+          I am interested in the migration of highly reliable applications movement towards API/Web front-end architecture and how quantum computers could change the future of software development and aid us in our fight against climate change.
+          </div>
+          {/* <button onClick={'https://github.com/BenHolmes/benholmes.github.io/raw/main/src/assets/images/Holmes_Resume_v4.5.pdf'}>Download Resume</button> */}
         </div>
       </div>
 
@@ -26,7 +30,7 @@ export default function Home() {
             ${profileSm} 600w,
             ${profileLg} 800w,
             ${profileLg} 1200w`}
-          //sizes ="(min-width:1336px) 75vw, 50vw"
+          sizes ="(min-width:1336px) 75vw, 50vw"
           alt="Ben Holmes" 
         />
       </div>
